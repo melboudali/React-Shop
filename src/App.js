6c3a18c7c02f9af3f093daa38b270f737be5fd17
MedEBoudali <med.sneaky@gmail.com>
2020-05-27 07:45:09 +0000

    Names Changed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Container } from '@material-ui/core';
-import Navbar from './components/layouts/Navbar';
-import Home from './components/Home';
+import Navbar from './Components/layouts/Navbar';
+import Home from './Components/Home';
 import './App.scss';
 
 const App = () => (
