22da39f542a5299d1764c25e5bdf0bba866e1a8e
MedEBoudali <med.sneaky@gmail.com>
2020-05-28 20:27:51 +0000

    add routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import { Container } from '@material-ui/core';
 import Navbar from './Layouts/Navbar';
 import Home from './Pages/Home/Home';
@@ -7,7 +8,8 @@ import './App.scss';
 const App = () => (
   <>
     <Navbar Container={Container} />
-    <Home />
+    <Route exact path='/' component={Home} />
+    <Route path='/lol' component={() => <h1>Hello World</h1>} />
   </>
 );
 
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import Directory from '../../Components/Directory/Directory';
 import PropTypes from 'prop-types';
 
-const Home = props => {
+const Home = ({ hello }) => {
   return (
     <div className='Home'>
       <Directory />
+      <h1>{hello}</h1>
     </div>
   );
 };
