11bd91e896e34d7450079b4f2d136de89cbd5525
MedEBoudali <med.sneaky@gmail.com>
2020-05-29 08:56:49 +0000

    Switched from sass to scss

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,15 +2,11 @@ import React from 'react';
 import Directory from '../../Components/Directory/Directory';
 import PropTypes from 'prop-types';
 
-const Home = props => {
-  console.log(props.match.params);
-  console.log(props);
-  return (
-    <div className='Home'>
-      <Directory />
-    </div>
-  );
-};
+const Home = props => (
+  <div className='Home'>
+    <Directory />
+  </div>
+);
 
 Home.propTypes = {};
 
