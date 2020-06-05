f26f231341fd78a35d7d8ad837b3d3c8acbc5c2b
MedEBoudali <med.sneaky@gmail.com>
2020-06-05 19:00:59 +0000

    Adding some styling

--- src/Layouts/Navbar.jsx
+++ src/Layouts/Navbar.jsx
@@ -174,7 +174,7 @@ const Navbar = ({ Container, history }) => {
                 FAQS
               </Typography>
               <Typography className='NavbarMenu' variant='h7'>
-                CONTACT
+                CONTACT US
               </Typography>
               <IconButton
                 className='profile'
