9a4e0497ea3b4011d9fcea46e56cbbf6093e2455
MedEBoudali <med.sneaky@gmail.com>
2020-06-25 11:00:11 +0000

    small change again

--- src/Layouts/Announcement/Announcement.jsx
+++ src/Layouts/Announcement/Announcement.jsx
@@ -6,7 +6,7 @@ const Announcement = () => (
     <h1>
       We are experiencing international delivery delays in all destinations due to limited airline
       capacity and government restrictions as a result of COVID-19. Some customers may experience a
-      delay of 5-7 weeks.
+      delay of 4-6 weeks.
     </h1>
   </div>
 );
