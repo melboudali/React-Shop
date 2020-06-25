791b3acb8b6d9deaad8098d3513ee77fdfc31483
MedEBoudali <med.sneaky@gmail.com>
2020-06-25 11:59:45 +0000

    deploying build folder

---
+++ build/precache-manifest.8f3e6ba37a7999baa5979a2cb0793a5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ee6af07d1283b5609155585af0b1228",
+    "url": "./index.html"
+  },
+  {
+    "revision": "75b6fbff3911ac11f778",
+    "url": "./static/css/main.4d94de84.chunk.css"
+  },
+  {
+    "revision": "5ac6a108722b421768bb",
+    "url": "./static/js/2.bee80f33.chunk.js"
+  },
+  {
+    "revision": "fbfb5e42a41e7004782949e93fdf296a",
+    "url": "./static/js/2.bee80f33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75b6fbff3911ac11f778",
+    "url": "./static/js/main.2c91fd0f.chunk.js"
+  },
+  {
+    "revision": "85c4937c54139f9312c9",
+    "url": "./static/js/runtime-main.85953690.js"
+  }
+]);
