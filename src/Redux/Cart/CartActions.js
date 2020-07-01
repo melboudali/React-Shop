95a7fc1d2d6196c362efa2b8bb7c21c12b864bf3
MedEBoudali <med.sneaky@gmail.com>
2020-07-01 23:34:35 +0000

    fixing some errors with redux logger

--- src/Redux/Cart/CartActions.js
+++ src/Redux/Cart/CartActions.js
@@ -4,9 +4,7 @@ export const addItemToCart = item => dispatch =>
   dispatch({ type: ADD_ITEM_TO_CART, payload: item });
 
 export const deleteItem = (id, quantity) => dispatch => {
-  if (quantity > 1) {
-    dispatch({ type: REDUCE_ITEM_QUANTITY, payload: id });
-  } else {
-    dispatch({ type: DELETE_ITEM_FROM_CART, payload: id });
-  }
+  quantity > 1
+    ? dispatch({ type: REDUCE_ITEM_QUANTITY, payload: id })
+    : dispatch({ type: DELETE_ITEM_FROM_CART, payload: id });
 };
