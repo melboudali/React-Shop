f86698e5c6f9f7fdf4ab424943138af4c4fd5d8b
MedEBoudali <med.sneaky@gmail.com>
2020-07-08 22:12:34 +0000

    remove logger & devtools-ext from production

--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -7,15 +7,17 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import RootReducer from './RootReducer';
 
 // Logger must be the last or we get undefined actions
-const middlewares = [ReduxThunk];
+const middlewares = [ReduxThunk, logger];
 
 // remove redux-logger & redux-devtools-extension from production
-const Store = createStore(
-  RootReducer,
-  process.env.NODE_ENV === 'production'
-    ? applyMiddleware(...middlewares)
-    : composeWithDevTools(applyMiddleware(...middlewares, logger))
-);
+// const Store = createStore(
+//   RootReducer,
+//   process.env.NODE_ENV === 'production'
+//     ? applyMiddleware(...middlewares)
+//     : composeWithDevTools(applyMiddleware(...middlewares, logger))
+// );
+
+const Store = createStore(RootReducer, composeWithDevTools(applyMiddleware(...middlewares)));
 
 const Persistor = persistStore(Store);
 
