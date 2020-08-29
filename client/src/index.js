7e9c323c205e458f511235c5d7774a70ddcb41b7
MedEBoudali <med.sneaky@gmail.com>
2020-08-29 22:02:18 +0000

    add apollo

--- client/src/index.js
+++ client/src/index.js
@@ -4,17 +4,50 @@ import { Provider } from 'react-redux';
 import { Store, Persistor } from './Redux/Store';
 import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
+import { ApolloProvider } from 'react-apollo';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloClient, gql } from 'apollo-boost';
 import ScrollToTop from './Utils/ScrollToTop';
 import App from './App';
 
+const link = createHttpLink({
+  uri: 'https://crwn-clothing.com'
+});
+
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({ link, cache });
+
+client
+  .query({
+    query: gql`
+      {
+        getCollectionsByTitle(title: "hats") {
+          id
+          title
+          items {
+            id
+            name
+            price
+            imageUrl
+          }
+        }
+      }
+    `
+  })
+  .then(res => console.log(res));
+
 ReactDOM.render(
-  <Provider store={Store}>
-    <BrowserRouter>
-      <PersistGate persistor={Persistor}>
-        <ScrollToTop />
-        <App />
-      </PersistGate>
-    </BrowserRouter>
-  </Provider>,
+  <ApolloProvider client={client}>
+    <Provider store={Store}>
+      <BrowserRouter>
+        <PersistGate persistor={Persistor}>
+          <ScrollToTop />
+          <App />
+        </PersistGate>
+      </BrowserRouter>
+    </Provider>
+  </ApolloProvider>,
   document.getElementById('root')
 );
