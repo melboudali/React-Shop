b621d4123ff06a664e90b4d1341e9929e66eef0d
MedEBoudali <med.sneaky@gmail.com>
2020-09-07 12:50:59 +0000

    graphql deleted

--- client/src/index.js
+++ client/src/index.js
@@ -4,42 +4,10 @@ import { Provider } from 'react-redux';
 import { Store, Persistor } from './Redux/Store';
 import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
-import { ApolloProvider } from 'react-apollo';
-import { createHttpLink } from 'apollo-link-http';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { ApolloClient, gql } from 'apollo-boost';
 import ScrollToTop from './Utils/ScrollToTop';
 import App from './App';
 
-const link = createHttpLink({
-  uri: 'https://crwn-clothing.com'
-});
-
-const cache = new InMemoryCache();
-
-const client = new ApolloClient({ link, cache });
-
-client
-  .query({
-    query: gql`
-      {
-        getCollectionsByTitle(title: "hats") {
-          id
-          title
-          items {
-            id
-            name
-            price
-            imageUrl
-          }
-        }
-      }
-    `
-  })
-  .then(res => console.log(res));
-
 ReactDOM.render(
-  <ApolloProvider client={client}>
     <Provider store={Store}>
       <BrowserRouter>
         <PersistGate persistor={Persistor}>
@@ -47,7 +15,6 @@ ReactDOM.render(
           <App />
         </PersistGate>
       </BrowserRouter>
-    </Provider>
-  </ApolloProvider>,
+    </Provider>,
   document.getElementById('root')
 );
