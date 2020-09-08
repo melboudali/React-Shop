81c2ec135df2ead6441345040d2c89fdacba5dfd
MedEBoudali <med.sneaky@gmail.com>
2020-09-08 22:40:52 +0000

    add compression for heroku

--- Server.js
+++ Server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
+const compression = require('compression');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -10,6 +11,7 @@ process.env.NODE_ENV !== 'production' && require('dotenv').config();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(compression());
 app.use(cors());
 
 // @Routes
