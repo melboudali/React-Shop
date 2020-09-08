a1c12bfcc7e63487659183497c0b81832c1b5ddb
MedEBoudali <med.sneaky@gmail.com>
2020-09-08 23:21:23 +0000

    add compression for heroku

--- client/src/App.js
+++ client/src/App.js
@@ -40,7 +40,7 @@ const App = ({ CheckUserSession }) => {
               <Route path='/shop' component={ShopPage} />
               <Route exact path='/checkout' component={CheckoutPage} />
               {/* 404 Not Found Route */}
-              <Route path='*' component={NotFoundPage} />
+              <Route exact path='*' component={NotFoundPage} />
             </Switch>
           </Suspense>
         </ErrorBoundary>
