e4d8a18002e73bb0bea6ab8d2411a21e6b405d45
Med EL BOUDALI <53059156+MedElBoudali@users.noreply.github.com>
2020-09-08 23:04:26 +0000

    fixing errors

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { Store, Persistor } from './Redux/Store';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
 import ScrollToTop from './Utils/ScrollToTop';
 import App from './App';
@@ -10,10 +10,10 @@ import App from './App';
 ReactDOM.render(
   <Provider store={Store}>
     <PersistGate persistor={Persistor}>
-      <Router>
+      <BrowserRouter>
         <ScrollToTop />
         <App />
-      </Router>
+      </BrowserRouter>
     </PersistGate>
   </Provider>,
   document.getElementById('root')
