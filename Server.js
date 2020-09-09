0ea0c7819db4dbc6cffb607621c200833275e45a
MedEBoudali <med.sneaky@gmail.com>
2020-09-09 23:22:00 +0000

    add pwa

--- Server.js
+++ Server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const compression = require('compression');
 const app = express();
+const enforce = require('express-sslify');
 const port = process.env.PORT || 5000;
 
 process.env.NODE_ENV !== 'production' && require('dotenv').config();
@@ -13,10 +14,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(compression());
 app.use(cors());
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 
 // @Routes
 app.use('/payment', require('./routes/Payment.Route'));
 app.use('/cart', require('./routes/Cart.Route'));
+
+// Service Worker
+app.get('/service-worker', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
+});
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
