44dfdae688217547fda56405dce31aa0fc8a24e5
MedEBoudali <med.sneaky@gmail.com>
2020-10-04 12:05:28 +0000

    styling delete button on CartItem - Dropdown

--- client/src/Components/Navbar/CartItem/CartItem.jsx
+++ client/src/Components/Navbar/CartItem/CartItem.jsx
@@ -9,7 +9,7 @@ const CartItem = ({ Item: { id, name, firstImageUrl, newPrice, quantity }, delet
     <div className='CartItem'>
       <div className='ImgContainer' style={{ backgroundImage: `url(${firstImageUrl})` }} />
       <div className='ItemDetails'>
-        <span className='CloseButton' onClick={() => deleteItem(id, quantity)}>
+        <span className='DeleteButton' onClick={() => deleteItem(id, quantity)}>
           <svg viewBox='0 0 24 24'>
             <path stroke='none' d='M0 0h24v24H0z' />
             <line x1='4' y1='7' x2='20' y2='7' />
